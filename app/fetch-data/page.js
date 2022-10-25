8ab1dcba8b8975cd110d5d31f5f00367cdb31604
Hammed Oyedele <itz.harmid@gmail.com>
2022-10-25 21:04:56 +0100

    docs: add more examples for data fetching

--- app/fetch-data/page.js
+++ app/fetch-data/page.js
@@ -1,8 +1,25 @@
-// The return value is *not* serialized
-// You can return Date, Map, Set, etc.
 export async function getGithubData() {
+  // The default is static data fetching
+  //  This is equivalent to getStaticProps().
   const res = await fetch('https://api.github.com/users/devhammed');
 
+  // You can enable dynamic data fetching by using "no-store"
+  // This is equivalent to getServerSideProps().
+  // const resDynamic = await fetch('https://api.github.com/users/devhammed', {
+  //   cache: 'no-store',
+  // });
+
+  // To revalidated cached data, you can use the next.revalidate option in fetch().
+  // The default unit is seconds.
+  // This is equivalent to Incremental Static Regeneration (ISR).
+  // const resIncremental = await fetch('https://api.github.com/users/devhammed', {
+  //   next: {
+  //     revalidate: 10,
+  //   },
+  // });
+
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
   return res.json();
 }
 
