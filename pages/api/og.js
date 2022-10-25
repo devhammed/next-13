6f2f84eb819ca6aba752cca8aa62bdacb56cd848
Hammed Oyedele <itz.harmid@gmail.com>
2022-10-25 21:44:32 +0100

    feat: add image generation demo

---
+++ pages/api/og.js
@@ -0,0 +1,47 @@
+import { ImageResponse } from '@vercel/og';
+import { NextRequest } from 'next/server';
+
+export const config = {
+  runtime: 'experimental-edge',
+};
+
+export default async function handler(req) {
+  const { searchParams } = req.nextUrl;
+  const username = searchParams.get('username');
+
+  if (!username) {
+    return new Response('Get out of here!', { status: 400 });
+  }
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          fontSize: 60,
+          color: 'black',
+          background: '#f6f6f6',
+          width: '100%',
+          height: '100%',
+          paddingTop: 50,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <img
+          width='256'
+          height='256'
+          src={`https://github.com/${username}.png`}
+          style={{
+            borderRadius: 128,
+          }}
+        />
+        <p>github.com/{username}</p>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
+}
