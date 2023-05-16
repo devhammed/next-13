b3434fbbbc2ed3b15c13dba779f17a1da5ad56f5
Hammed Oyedele <itz.harmid@gmail.com>
2023-05-17 00:15:10 +0100

    feat: add error handling to fetch

--- app/github/[username]/page.tsx
+++ app/github/[username]/page.tsx
@@ -11,6 +11,10 @@ export interface PageProps {
 export async function getUserData(username: string) {
   const response = await fetch(`https://api.github.com/users/${username}`);
 
+  if (!response.ok) {
+    throw new Error('Unable to get user details from GitHub!');
+  }
+
   const data = await response.json();
 
   const user = await GitHubUserSchema.parseAsync(data);
