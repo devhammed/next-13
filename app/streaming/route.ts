a96719dbbc82ab92347b2535a8ee3021ad025049
Hammed Oyedele <itz.harmid@gmail.com>
2023-05-16 23:19:33 +0100

    refactor: remove the need to pass text encoder

--- app/streaming/route.ts
+++ app/streaming/route.ts
@@ -1,19 +1,24 @@
 import { sleep } from '@/utils/sleep';
 import { iteratorToStream } from '@/utils/stream';
 
-async function* makeIterator(encoder: TextEncoder): AsyncGenerator<Uint8Array> {
+async function* makeIterator(): AsyncGenerator<Uint8Array> {
+  const encoder = new TextEncoder();
+
   await sleep(2000);
+
   yield encoder.encode('<p>One</p>');
+
   await sleep(2000);
+
   yield encoder.encode('<p>Two</p>');
+
   await sleep(2000);
+
   yield encoder.encode('<p>Three</p>');
 }
 
 export async function GET(): Promise<Response> {
-  const encoder = new TextEncoder();
-
-  const iterator = makeIterator(encoder);
+  const iterator = makeIterator();
 
   const stream = iteratorToStream(iterator);
 
