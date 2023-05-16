af64b05ba98ab4f52fc743890ad78191219f5f5d
Hammed Oyedele <itz.harmid@gmail.com>
2023-05-17 00:01:43 +0100

    feat: add loading uis

---
+++ app/github/[username]/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className='flex flex-col justify-center items-center min-h-screen'>
+      <h1 className='text-3xl font-bold mb-10'>
+        Crunching latest profile data...
+      </h1>
+    </div>
+  );
+}
---
+++ app/github/og/[username]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className='flex flex-col justify-center items-center min-h-screen'>
+      <h1 className='text-3xl font-bold mb-10'>Generating OG...</h1>
+    </div>
+  );
+}
