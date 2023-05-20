32e0e73ac4f10511be1d463ef3f2e6b09da3a9e0
Hammed Oyedele <itz.harmid@gmail.com>
2023-05-20 20:01:41 +0100

    feat: add form demo to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,20 @@ export default function Home() {
         />
       </div>
 
-      <div className='mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left'>
+      <div className='mb-32 grid text-center lg:mb-0 lg:grid-cols-5 lg:text-left'>
+        <Link
+          href='/form'
+          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'>
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Form{' '}
+            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Using server actions to handle forms.
+          </p>
+        </Link>
         <Link
           href='/throw-error'
           className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'>
@@ -53,10 +66,10 @@ export default function Home() {
         />
 
         <GitHubFormModal
-          title='Image Generation'
+          title='Generate Image'
           link='/github/og/:username'
           subtitle='View image generation demo.'
-          prompt='Enter your GitHub username to generate an OG image.'
+          prompt='Enter your GitHub username to generate an image.'
         />
       </div>
     </main>
